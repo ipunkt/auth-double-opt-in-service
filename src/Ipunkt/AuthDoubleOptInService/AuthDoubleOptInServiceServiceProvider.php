45f55707cd280b44a9e470211ceb8e8a821433d0
Sven Speckmaier <sps@ipunkt.biz>
2015-01-16 15:04:26 +0100

    Package creation

---
+++ src/Ipunkt/AuthDoubleOptInService/AuthDoubleOptInServiceServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Ipunkt\AuthDoubleOptInService;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthDoubleOptInServiceServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
